a7db26b66322d093da0b9519c054fc2f25ed18c6
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-06-17 19:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+const firstName = document.getElementById("fname").value;
+  const lastName = document.getElementById("lname").value;
 
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
